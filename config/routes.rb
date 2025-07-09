e3624260da5a2e8a1a8db870ea9d3e0b0d7bad45
LuanGonc <luan.gonc.oficial@gmail.com>
2025-07-09 13:31:21 -0300

    Implementada uma página inicial(Home) para o sistema com links para as páginas de Notebooks, Colaboradores e Empréstimos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :loans
   resources :collaborators
   resources :equipment
@@ -9,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   root "home#index"
 end
